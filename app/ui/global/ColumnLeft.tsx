72355780b63ac31d003a95ed5e1f5ff56128d307
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-05-15 11:28:45 +0200

    came up with logo and added it to layout

--- app/ui/global/ColumnLeft.tsx
+++ app/ui/global/ColumnLeft.tsx
@@ -2,11 +2,13 @@ import ThemeButton from "./ThemeButton";
 import ProfileNav from "./ProfileNav";
 import HomeNav from "./HomeNav";
 import BookmarksNav from "./BookmarksNav";
+import Logo from "./Logo";
 
 const LeftColumn = () => {
     return (
         <nav className="w-48 px-4 flex-grow-0 flex-shrink-0 relative">
-            <ul className="mt-20 ml-4 flex flex-col gap-1 fixed">
+            <ul className="mt-5 ml-4 flex flex-col gap-1 fixed">
+                <Logo />
                 <HomeNav />
                 <ProfileNav />
                 <BookmarksNav />
