451a340d4742397d194a07210d53b01552f7f2a7
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-05-18 21:14:10 +0200

    fix

--- app/(with-layout)/tweet/[id]/page.tsx
+++ app/(with-layout)/tweet/[id]/page.tsx
@@ -1,17 +1,19 @@
 import { getTweetById } from "@/app/data/tweetData"
+import { getUserProfile } from "@/app/data/userData";
 import BackHeader from "@/app/ui/global/BackHeader"
 import TweetComponent from "@/app/ui/tweet/Tweet"
 import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: number } }) {
 
+    const { id } = await getUserProfile();
     const tweet = await getTweetById(params.id);
     if (!tweet) notFound();
 
     return (
         <div>
             <BackHeader />
-            <TweetComponent tweet={tweet} />
+            <TweetComponent userId={id} tweet={tweet} />
         </div>
     )
 }
