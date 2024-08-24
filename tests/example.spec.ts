8853baba6ffa9268bdd168ebfa8dd046240a2997
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-08-24 17:48:41 +0200

    debugging

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+test('debugging', async ({ page }) => {
+  await page.goto(`${process.env.BASE_URL}/`);
+  console.log('Page content:', await page.content()); // Log the entire HTML content
+  await expect(page).toHaveTitle("X++");
+});
+
 test('login page renders', async ({ page }) => {
   await page.goto('/');
 
