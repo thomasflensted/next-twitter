a1a9c76f9d9816546f63ec948bc15033d9de4a47
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-08-24 18:27:36 +0200

    think I got it

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,7 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('login page renders', async ({ page }) => {
   await page.goto(`${process.env.BASE_URL}/`);
-  console.log("BASE URL:", process.env.BASE_URL)
-
-
-  // Now continue with your test
-  // await expect(page).toHaveTitle("X++");
+  await page.waitForSelector('h2:has-text("Get Started")', { timeout: 10000 });
+  await expect(page).toHaveTitle("X++");
 });
