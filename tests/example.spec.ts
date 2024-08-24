9dbb4b34cc8e8161d0c64796a3e154c2257a9a67
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-08-24 18:17:03 +0200

    debugging

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,8 +3,7 @@ import { test, expect } from '@playwright/test';
 test('login page renders', async ({ page }) => {
   await page.goto(`${process.env.BASE_URL}/`);
 
-  // Wait for 3 seconds to give the Vercel page time to disappear
-  await page.waitForTimeout(3000);
+  await page.waitForSelector('h2:has-text("Get Started")', { timeout: 10000 });
 
   // Now continue with your test
   await expect(page).toHaveTitle("X++");
