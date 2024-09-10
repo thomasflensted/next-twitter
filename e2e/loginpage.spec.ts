d5079e39f96b3712a83918e31e594dcd59eaf6c4
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-09-10 21:13:06 +0200

    expanded tests on login page

--- e2e/loginpage.spec.ts
+++ e2e/loginpage.spec.ts
@@ -10,7 +10,7 @@ test.describe('Initial tests', () => {
         await expect(page).toHaveTitle("X++");
     });
 
-    test('Sign up form has appropriate field and button', async ({ page }) => {
+    test('Sign up form has appropriate fields and button', async ({ page }) => {
         // Check if the form exists
         const form = await page.locator('form');
         await expect(form).toBeVisible();
@@ -25,6 +25,16 @@ test.describe('Initial tests', () => {
         const submitBtn = page.locator('button[type="submit"]');
         await expect(submitBtn).toBeVisible();
         await expect(submitBtn).toHaveText('Sign Up');
-
     });
+
+    test('Locate and click sign in link, check if url is correct', async ({ page }) => {
+        const signInLink = page.locator('a[href="/signin"]');
+        await Promise.all([
+            expect(signInLink).toBeVisible(),
+            expect(signInLink).toHaveText('Sign in here.'),
+            signInLink.click()
+        ])
+        await page.waitForURL('/signin')
+    })
+
 });
