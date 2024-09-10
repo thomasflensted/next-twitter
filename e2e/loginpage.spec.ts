ced9018787f35721aff26f1f58ee47a935dd8f6a
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-09-10 20:57:35 +0200

    sign up page tests

--- e2e/loginpage.spec.ts
+++ e2e/loginpage.spec.ts
@@ -1,8 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test('login page renders', async ({ page }) => {
-    await page.goto(process.env.BASE_URL || '/'); // Use the BASE_URL here
+test.beforeEach(async ({ page }) => {
+    await page.goto(process.env.BASE_URL || '/');
+})
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle("X++");
+test.describe('Initial tests', () => {
+
+    test('page loads', async ({ page }) => {
+        await expect(page).toHaveTitle("X++");
+    });
+
+    test('Sign up form has appropriate field and button', async ({ page }) => {
+        // Check if the form exists
+        const form = await page.locator('form');
+        await expect(form).toBeVisible();
+
+        const fieldNames = ['email', 'password', 'confirmpw'];
+        for (const fieldName of fieldNames) {
+            const field = page.locator(`input[name="${fieldName}"]`)
+            await expect(field).toBeVisible();
+            await expect(field).toHaveAttribute('name', fieldName);
+        }
+
+        const submitBtn = page.locator('button[type="submit"]');
+        await expect(submitBtn).toBeVisible();
+        await expect(submitBtn).toHaveText('Sign Up');
+
+    });
 });
