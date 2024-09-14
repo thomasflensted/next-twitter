ef4ff0131790d7fc43094bc43455273fceb9563e
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-09-14 12:27:34 +0200

    new functions to follow status and is_myself

--- app/lib/api/followers.ts
+++ app/lib/api/followers.ts
@@ -1,17 +1,20 @@
 import { createClient } from "@/utils/supabase/server";
+import { getUserId } from "./users";
 
 export async function getFollowers(handle: string) {
     const supabase = createClient()
+    const userId = await getUserId();
     const res = await supabase
-        .rpc('get_followers',
-            { user_handle: handle });
+        .rpc('get_followers_of_handle',
+            { target_handle: handle, current_user_id: userId });
     return res;
 }
 
 export async function getFollowing(handle: string) {
     const supabase = createClient()
+    const userId = await getUserId();
     const res = await supabase
-        .rpc('get_following',
-            { user_handle: handle });
+        .rpc('get_users_followed_by_handle',
+            { target_handle: handle, current_user_id: userId });
     return res;
 }
