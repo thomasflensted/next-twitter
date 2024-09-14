bff09b495ec0dc2724e10b8070e935cb05ba31a9
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-09-14 07:29:42 +0200

    expanded component

--- app/ui/login/LabelAndInput.tsx
+++ app/ui/login/LabelAndInput.tsx
@@ -7,17 +7,37 @@ type Props = {
     error?: string[] | null | undefined
     children?: ReactNode
     bg?: string
+    preface?: string
+    placeholder?: string
+    textarea?: boolean
 }
 
-const LabelAndInput = ({ name, label, error = null, type = 'text', children, bg = 'bg-white' }: Props) => {
+const LabelAndInput = ({
+    name,
+    label,
+    error,
+    type = 'text',
+    children,
+    bg = 'bg-white',
+    placeholder,
+    textarea,
+}: Props) => {
+
     return (
         <div className="relative">
             <label className={`font-light -top-2 left-2 px-1 text-xs absolute text-emerald-500 ${bg}`}>{label}</label>
-            <input
+            {!textarea ? <input
                 type={type}
                 name={name}
+                defaultValue={placeholder}
                 className={`${error ? 'border-red-500' : ''} ${bg}
-                    border w-full rounded py-2 px-2 text-xs font-light focus:outline-none focus:ring-1 focus:ring-emerald-600`} />
+                    border w-full rounded py-2 px-3 text-xs font-light focus:outline-none focus:ring-1 focus:ring-emerald-600`} />
+                : <textarea
+                    rows={4}
+                    name={name}
+                    defaultValue={placeholder}
+                    className={`${error ? 'border-red-500' : ''} ${bg} border px-3 resize-none w-full rounded py-2 text-xs font-light focus:outline-none focus:ring-1 focus:ring-emerald-600 `}
+                />}
             {children}
         </div>
     )
