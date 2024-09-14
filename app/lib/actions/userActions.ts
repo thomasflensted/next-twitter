9f1551499f383e9aa7d3de5ad44df4472b2d910c
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-09-14 15:46:15 +0200

    code to upload to s3

--- app/lib/actions/userActions.ts
+++ app/lib/actions/userActions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from "next/cache";
 import { getUserId } from "../api/users";
 import { ProfileSchema } from "../validation/profileSchema";
 import { redirect } from "next/navigation";
+import { ImageSchema } from "../validation/imageSchema";
+import { uploadImageToS3 } from "../s3Bucket";
 
 export async function unfollowUser(userHandle: string) {
     const supabase = createClient()
@@ -39,8 +41,8 @@ export async function followUser(userHandle: string) {
 
 export type AccountState = {
     error: {
-        profile_pic?: string[],
-        cover_photo?: string[],
+        profilepic?: string[],
+        coverphoto?: string[],
         name?: string[],
         handle?: string[],
         bio?: string[],
@@ -54,32 +56,54 @@ export async function updateAccount(
     prevState: AccountState,
     formData: FormData): Promise<AccountState> {
 
-    const res = ProfileSchema.safeParse({ ...Object.fromEntries(formData) });
+    const res = ProfileSchema.extend({
+        coverphoto: ImageSchema,
+        profilepic: ImageSchema
+    }).safeParse(Object.fromEntries(formData));
 
     if (!res.success) {
         return {
             error: {
                 ...res.error.flatten().fieldErrors,
-                general: 'Validation failed'
+                general: ''
             }
         }
+    }
+
+    const supabase = createClient()
+    const userId = await getUserId();
+
+    const handleRes = await supabase
+        .from('accounts')
+        .select('handle')
+        .eq('handle', res.data.handle)
+        .neq('user_id', userId)
+
+    if (handleRes.error) {
+        return { error: { general: "Something went wrong" } }
+    } else if (handleRes.data.length > 0) {
+        return { error: { handle: ["Another user with that handle already exists"] } }
+    }
+
+    const { data } = res;
+    const profilePicUrl = data.profilepic ? await uploadImageToS3(data.profilepic) : null;
+    const coverPhotoUrl = data.coverphoto ? await uploadImageToS3(data.coverphoto) : null;
+
+    const { error } = await supabase
+        .from('accounts')
+        .update({
+            name: data.name,
+            handle: data.handle,
+            bio: data.bio,
+            location: data.location,
+            website: data.website,
+            profile_pic: profilePicUrl?.url.split('?')[0],
+            cover_photo: coverPhotoUrl?.url.split('?')[0]
+        })
+        .eq('user_id', userId);
+    if (error) {
+        return { error: { general: error.message } }
     } else {
-        const supabase = createClient()
-        const userId = await getUserId();
-        const { error } = await supabase
-            .from('accounts')
-            .update({
-                name: res.data.name,
-                handle: res.data.handle,
-                bio: res.data.bio,
-                location: res.data.location,
-                website: res.data.website,
-            })
-            .eq('user_id', userId);
-        if (error) {
-            return { error: { general: error.message } }
-        } else {
-            redirect('/' + res.data.handle);
-        }
+        redirect('/' + res.data.handle);
     }
 }
