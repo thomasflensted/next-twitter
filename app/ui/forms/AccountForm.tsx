7d162d34304d94bd7e3fe9e843219a49b6edd50e
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-09-14 15:46:39 +0200

    profilepic and coverphoto in sep components

--- app/ui/forms/AccountForm.tsx
+++ app/ui/forms/AccountForm.tsx
@@ -5,6 +5,8 @@ import LabelAndInput from "../login/LabelAndInput"
 import SubmitBtn from "../login/SubmitBtn"
 import { useFormState } from "react-dom"
 import { useRouter } from "next/navigation"
+import CoverPhotoInput from "./CoverPhotoInput"
+import ProfilePicInput from "./ProfilePicInput"
 
 type Props = {
     name: string,
@@ -16,7 +18,7 @@ type Props = {
     cover_photo: string | null
 }
 
-const AccountForm = ({ ...data }: Props) => {
+const AccountForm = (data: Props) => {
 
     const initialState: AccountState = { error: null }
     const [state, action] = useFormState(updateAccount, initialState)
@@ -24,18 +26,12 @@ const AccountForm = ({ ...data }: Props) => {
 
     return (
         <form id="account" action={action} className="w-11/12 flex flex-col gap-6 mb-12">
-            <div className="flex flex-col gap-4 items-center">
-                <div className="w-28 rounded-full h-28 bg-gray-100 relative overflow-hidden">
-                    {data.profile_pic && <img className="h-full" src={data.profile_pic} alt="profile picture" />}
-                </div>
-                {/* <div className="h-6 w-6 rounded-full border bg-gray-50 absolute bottom-2 right-1"></div> */}
-            </div>
-            <div className="flex flex-col gap-1">
-                <div className="w-full rounded border h-40 bg-gray-100 overflow-hidden">
-                    {data.cover_photo && <img className="object-fit" src={data.cover_photo} alt="cover photo" />}
-                </div>
-                <button className="bg-gray-50 text-emerald-600 w-full hover:bg-gray-100 transition-colors border rounded py-1.5">Select Cover Photo</button>
-            </div>
+            <ProfilePicInput fileUrl={data.profile_pic}>
+                {state.error?.profilepic && <p className="text-red-500 font-light text-xs">{state.error.profilepic[0]}</p>}
+            </ProfilePicInput>
+            <CoverPhotoInput fileUrl={data.cover_photo}>
+                {state.error?.coverphoto && <p className="text-red-500 font-light mt-1 text-xs">{state.error.coverphoto[0]}</p>}
+            </CoverPhotoInput>
             <LabelAndInput error={state.error?.name} placeholder={data.name} bg="bg-gray-50" name="name" label="Name">
                 {state.error?.name && <p className="text-red-500 font-light mt-1 ml-1 text-xs">{state.error.name[0]}</p>}
             </LabelAndInput>
@@ -51,6 +47,9 @@ const AccountForm = ({ ...data }: Props) => {
             <LabelAndInput error={state.error?.website} placeholder={data.website ? data.website : ''} bg="bg-gray-50" name="website" label="Website">
                 {state.error?.website && <p className="text-red-500 font-light mt-1 ml-1 text-xs">{state.error.website[0]}</p>}
             </LabelAndInput>
+            <div className="text-left">
+                {state.error?.general && <p className="text-red-500 font-medium text-xs">{state.error.general}</p>}
+            </div>
             <div className="flex gap-1">
                 <button onClick={() => router.back()} className="w-full border font-medium rounded py-1.5 text-red-500 hover:bg-gray-100">Cancel</button>
                 <SubmitBtn formId="account" label="Save" />
