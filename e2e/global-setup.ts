90d3f9843aef7dc0bf6842be87808d31910d031c
thomasflensted <74050633+thomasflensted@users.noreply.github.com>
2024-09-17 20:54:38 +0200

    add debugging statement

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -33,6 +33,7 @@ async function signUpAndSaveState(browserType: BrowserType, browserName: 'chromi
     await confirmField.fill(credentials[browserName].password);
     await submitBtn.click();
 
+    console.log('BASE_URL:', process.env.BASE_URL);
     await page.waitForURL('**/account', { timeout: 20000 });
 
     await context.storageState({ path: `./e2e/auth-state/${browserName}-storage-state.json` });
